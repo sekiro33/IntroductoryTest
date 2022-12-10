69cc806f4384da286b0fd145698e605a25f7fc53
sekiro33 <aleksejmakismov1@gmail.com>
2022-12-10 17:35:14 +0400

    Add project

---
+++ IntroductoryTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+class Program
+{
+    private static void Main()
+    {
+        var sb = new StringBuilder();
+
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                sb.Append("Fizz");
+            }
+
+            if (i % 5 == 0)
+            {
+                sb.Append("Buzz");
+            }
+
+            Console.WriteLine(sb.Length > 0 ? sb.ToString() : i);
+
+            sb.Clear();
+        }
+    }
+}
